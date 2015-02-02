67f8f4f7d72273d994911bdbcdf364df4909cdc3
Haney Soares Silva <haneysilva@gmail.com>
2015-02-02 10:41:48 -0200

    Criação da fábrica

---
+++ app/src/main/java/com/trio/ifg/catchingballs/game/Fabrica.java
@@ -0,0 +1,16 @@
+package com.trio.ifg.catchingballs.game;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aluno on 02/02/2015.
+ */
+public class Fabrica {
+
+    public static ArrayList<Bola> novaListaDeBolas () {
+
+        ArrayList<Bola> listaDeBolas = new ArrayList<Bola>();
+
+        return listaDeBolas;
+    }
+}
