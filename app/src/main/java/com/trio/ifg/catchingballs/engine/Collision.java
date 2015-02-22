3dbc37e5c5183ddabc28b1169743789acb96c1c2
gustavohsr97 <gusttavohenrique@hotmail.com>
2015-02-21 22:03:00 -0200

    Update Collision.java
    Terminei a colisão, fiz um for de modo que ele percorra se a colisão ou não entre todas as bolas;

--- app/src/main/java/com/trio/ifg/catchingballs/engine/Collision.java
+++ app/src/main/java/com/trio/ifg/catchingballs/engine/Collision.java
@@ -1,20 +1,50 @@
 package com.trio.ifg.catchingballs.engine;
 
-import android.graphics.Rect;
+
+
+import com.trio.ifg.catchingballs.game.Bola;
+import com.trio.ifg.catchingballs.game.Fabrica;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by Haney on 07/01/2015.
+ * Created by Guatvo on 21/02/2015.
  */
 public class Collision{
+    int  x = 0;
 
     public Collision() {
     }
 
-    public static boolean verificarColisao(GameObject a, GameObject b) {
+    public static void verificarColisao() {
 
-        Rect rA = a.getRect();
-        Rect rB = b.getRect();
+        Fabrica fabricaBola = new Fabrica();
 
-        return rA.intersect(rB);
+        ArrayList<Bola> listaBolas = fabricaBola.novaListaDeBolas();
+
+
+        for (int i = 0; i < 12; i++) {
+            Bola bola1 = listaBolas.get(i);
+            for (int j = 0; j < 12; j++) {
+                Bola bola2 = listaBolas.get(j);
+
+                if (bola1 == bola2) {
+                    j = j + 1;
+                }
+                if (bola1.getX() == bola2.getX()) {
+                    bola1.inverterX();
+                    bola2.inverterX();
+                }
+                if (bola1.getY() == bola1.getY()) {
+                    bola1.inverterY();
+                    bola2.inverterY();
+                }
+            }
+
+        }
+
     }
+
 }
